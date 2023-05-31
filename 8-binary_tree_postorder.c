0776d29e75059c3c9f12f69c8cdc8d07b63cc022
Iman Hamza <iman.ali.hamza@gmail.com>
2023-05-31 14:15:30 -0700

    create 8-binary_tree_postorder.c

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - a function that goes through
+ * a binary tree using post-order traversal
+ *
+ * @tree: a pointer to the root node of the tree to traverse
+ * @func: a pointer to a function to call for each node
+ *
+ * Return: Nothing
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	/* recursively traverse the left subtree */
+	binary_tree_postorder(tree->left, func);
+
+	/* recursively traverse the right subtree */
+	binary_tree_postorder(tree->right, func);
+
+	/* Call the fuction to the current node */
+	func(tree->n);
+}
