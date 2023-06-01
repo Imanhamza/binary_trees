679387b887665072397464b7387c98bf2ae4192d
Iman Hamza <iman.ali.hamza@gmail.com>
2023-06-01 11:01:04 -0700

    add the grandpa in the count

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -21,18 +21,19 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 
 	/* Find the position of each node*/
 	/* if the they are the same node or one is the parent of the other */
-	if (first == second || first == second->parent ||
-			first->parent == second)
+	if (first == second)
 		return ((binary_tree_t *)first);
 
 	/*find the position of the node parent */
 	firstAn = binary_trees_ancestor(first, second->parent);
-	if (first->parent == NULL || first->parent == second->parent)
+	if (first->parent == NULL || first == second->parent ||
+			(!first->parent->parent && second->parent))
 		return (firstAn);
 
 	/* Find out if the second parent is NULL */
 	secondAn = binary_trees_ancestor(first->parent, second);
-	if (second->parent == NULL)
+	if (second->parent == NULL || second == first->parent ||
+			(!second->parent->parent && first->parent))
 		return (secondAn);
 
 	/* Recursivley call the function again for each parent */
